d97d2e010aaa7769f6d8a9a61fed61c6cf8843fb
han xu <hanxu27@gmail.com>
2019-04-16 19:15:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  output = {}
+  
+  data.each do |color, color_data|
+    color_data.each do |data, pigeons|
+      pigeons.each do |pigeon|
+        if !output.has_key?(pigeon)
+          output[pigeon] = {}
+        end
+        
+        if !output[pigeon].has_key?(color)
+          output[pigeon][color] = []
+        end
+        
+        if !output[pigeon][color].include?(data)
+          output[pigeon][color] << data.to_s
+        end
+      end
+    end
+  end
+  output
 end
